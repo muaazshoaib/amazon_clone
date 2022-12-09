405c136395a28bb0ee9ec07da8041043e5ea9163
Muaaz Shoaib <muaazshoaib1670@gmail.com>
2022-12-09 17:58:03 +0500

    Creating Server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,13 @@
 console.log('Hello, World');
 // print('Hello, World')
+const express = require('express');
+// import 'package:express/express.dart';
+const PORT = 3000;
+
+const app = express();
+
+// CREATING AN API
+app.listen(PORT, "0.0.0.0", () => {
+   console.log("Connected at port " + PORT);
+})
+// localhost
